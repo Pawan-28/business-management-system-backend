53ba2969c468b78e6b776b831708479c9cf1eed7
Pawan Kumar <kp355576@gmail.com>
2025-12-19 16:49:14 +0530

    ok

--- backend/settings.py
+++ backend/settings.py
@@ -152,6 +152,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # REST Framework Configuration
 REST_FRAMEWORK = {
+     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,  # 10 items per page
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
